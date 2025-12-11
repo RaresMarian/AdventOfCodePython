2cd2f81516e30c9eb4a1e2d4dc7a623c3d445f5c
Rares <raresmarian1996@gmail.com>
2025-12-11 20:45:08 +0100

    Add files via upload

---
+++ Day1_2025.py
@@ -0,0 +1,26 @@
+with open("./input_day1.txt", "r") as file:
+    contenuto = file.readlines()
+
+c=10
+
+somma=50
+password=0
+for x in contenuto:         
+    if(x[0]=="R"):
+        numero=int(x[1:])
+        somma= (somma+numero)%100
+        
+    else :
+        numero=int(x[1:])
+        somma= (somma-numero)%100
+
+    if(somma==0):
+        password+=1
+
+
+       
+
+print(password)
+            
+      
+    
